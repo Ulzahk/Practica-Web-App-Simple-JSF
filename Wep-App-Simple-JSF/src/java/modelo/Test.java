584051da8313d75730489ec2d79d3eab3950204e
Ulzahk <46821988+Ulzahk@users.noreply.github.com>
2020-02-24 14:42:51 -0500

    Estructura de Logeo Basica Completa

---
+++ Wep-App-Simple-JSF/src/java/modelo/Test.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ApplicationScoped;
+
+/**
+ *
+ * @author PRACTICANTE
+ */
+@ManagedBean
+@ApplicationScoped
+public class Test {
+    private String user, pwd;
+    /**
+     * Creates a new instance of Test
+     */
+    public Test() {
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+    
+    
+    
+}
